4b81a3a21c69ca520b7e9ad94bc5d1c51f325f8f
xiao <zghanxiao@gmail.com>
2014-11-05 15:36:59 -0800

    chapter8 cookie session

--- article/views.py
+++ article/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response
 from django.template import Context
 from django.views.generic.base import TemplateView
 from article.models import Article
+from django.http import HttpResponse
 def hello(request):
 	name = "Xiao"
 	html = "<html><body> Hi  %s this seems to have worked!</body></html>" % name
@@ -19,16 +20,35 @@ def hello_template(request):
 def hello_template_simple(request):
 	name = 'Xiao'
 	return render_to_response('hello.html', {'name':name})
+
 class HelloTemplate(TemplateView):
 	template_name = "hello_class.html"
 	def get_context_data(self, **kwargs):
 		context = super(HelloTemplate, self).get_context_data(**kwargs)
 		context['name'] = 'Xiao'
 		return context
+
 def articles(request):
+	language = "en-us"
+	session_language = "en-us"
+
+	if 'lang' in request.COOKIES:
+		language = request.COOKIES['lang']
+
+	if 'lang' in request.session:
+		session_language = request.session['lang']
+
 	return render_to_response('articles.html',
-				{'articles':Article.objects.all() })
+			{'articles':Article.objects.all(), 
+			'language': language,
+			'session_language' : session_language})
 
 def article(request, article_id=1):
 	return render_to_response('article.html',
 			{'article': Article.objects.get(id=article_id) })
+
+def language(request, language = 'en-us'):
+	response = HttpResponse("setting language to %s" % language)
+	response.set_cookie('lang', language)
+	request.session['lang'] = language
+	return response
