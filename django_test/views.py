cb678647fdf26ccb16db22bea101c114279e6463
xiao <zghanxiao@gmail.com>
2014-11-18 10:18:02 -0800

    register and extension

--- django_test/views.py
+++ django_test/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
+from forms import MyRegistrationForm
 
 def login(request):
 	c = {}
@@ -26,3 +28,18 @@ def invalid_login(request):
 def logout(request):
 	auth.logout(request)
 	return render_to_response('logout.html')
+
+def register_user(request):
+	if request.method == 'POST':
+		form = MyRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/accounts/register_success')
+	args = {}
+	args.update(csrf(request))
+	args['form'] = MyRegistrationForm()
+	print args
+	return render_to_response('register.html', args)
+
+def register_success(request):
+	return render_to_response('register_success.html')
